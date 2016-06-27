2c198fd13da704a2c36a70d367ce3ab11a6c4b37
Angela Liu <angelamuliu@gmail.com>
2016-06-27 13:50:06 -0400

    nav changes color on scroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 
 $(document).ready(function() {
 
+
+
+    // NAV: Change color of scroll to an almost black as I go down
+    $(window).scroll( function() {
+        var scrollposition = $(window).scrollTop();
+        if (scrollposition > 100) {
+            $("nav")[0].style.background ="rgba(34,38,43," + ((-100 + scrollposition)/200 ) + ")";
+        } else {
+            $("nav")[0].style.background = 'none';
+        }
+    })
+
+
+
     // Sticky double diamond
     var sectionHeightBefore = $("#diamonds").offset().top - 100; // height of sections before diamonds + nav
     var diamondHeight = $("#process-sections").height();
